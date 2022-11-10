302321aa8dba69b2c6aa826f9daf77e2065d62ad
Tolegen Timur <demigodkz@gmail.com>
2022-11-10 18:56:10 +0600

    feat: add authSlice for store

--- src/shared/store/index.ts
+++ src/shared/store/index.ts
@@ -1,9 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import { managerAPI } from 'shared/api';
+import authSlice from './model/authSlice';
+import langSlice from './model/langSlice';
 
 export const store = configureStore({
-  reducer: { [managerAPI.reducerPath]: managerAPI.reducer },
+  reducer: {
+    [managerAPI.reducerPath]: managerAPI.reducer,
+    authSlice: authSlice.reducer,
+    langSlice: langSlice.reducer,
+  },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(managerAPI.middleware),
 });
 
