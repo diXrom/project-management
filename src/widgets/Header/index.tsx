0d23d26941d5910c660eab0dd7e6785be198c8e2
Tolegen Timur <demigodkz@gmail.com>
2022-11-12 03:09:36 +0600

    feat: add Header component

--- src/widgets/Header/index.tsx
+++ src/widgets/Header/index.tsx
@@ -1,23 +1,38 @@
-import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { useTranslation } from 'react-i18next';
+import { VscProject } from 'react-icons/vsc';
+
+import LangSwitcher from 'widgets/Header/ui/LangSwitcher';
+import { useAppSelector } from 'shared/store/model/hooks';
+import { getUser } from 'shared/store/model/selectors';
 import { ROUTE_PATH } from 'shared/common/constants';
-import Button from 'shared/components/Button';
-import LangSwitcher from 'widgets/LangSwitcher/LangSwitcher';
+import AuthBtns from './ui/AuthBtns';
+import UserPanel from './ui/UserPanel';
+import useScrollY from './model/useScrollY';
+import clsx from 'clsx';
 
 const Header = () => {
-  const { t } = useTranslation();
+  const user = useAppSelector(getUser);
+  //Пока отлючена анимация перехода стики хедера
+  /*  const scrollY = useScrollY(); */
+  useTranslation();
+
   return (
-    <header className="flex content-center bg-white shadow-md ">
+    <header
+      className={clsx(
+        'sticky top-0 z-10 flex content-center shadow-md bg-black transition duration-500 select-none'
+        /*  scrollY > 90 ? 'bg-opacity-80' : 'bg-opacity-100' */
+      )}
+    >
       <div className="container flex justify-between mx-auto px-2.5 md:px-5">
-        <Link to="/" className="flex items-center">
-          <div>Logo</div>
+        <Link to={user ? ROUTE_PATH.BOARDS : ROUTE_PATH.INDEX} className="flex items-center">
+          <VscProject className="w-10 h-10 text-white transition duration-300 active:scale-95" />
         </Link>
-        <div className="items-center flex-shrink-0 gap-2.5 flex">
+        <motion.div layout className="items-center flex-shrink-0 gap-2.5 flex">
           <LangSwitcher />
-          <Link to={ROUTE_PATH.LOGIN}>
-            <Button>{t('signIn')}</Button>
-          </Link>
-        </div>
+          {user ? <UserPanel /> : <AuthBtns />}
+        </motion.div>
       </div>
     </header>
   );
