96e2d8c1c3fb98d8da772a0788246a93cfca1b2d
Inna <malinna13@gmail.com>
2022-11-12 01:06:17 +0600

    feat: boards loading, delete board

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,11 +1,32 @@
 import { motion } from 'framer-motion';
+import { useDeleteBoardMutation, useGetBoardsQuery } from 'shared/api/model/boardsSlice';
+import { useTranslation } from 'react-i18next';
 
 import { fade, motionVariants } from 'shared/common/styles';
+import Card from 'shared/components/Card';
+import Button from 'shared/components/Button';
+import { IBoardId } from 'shared/api/lib/types';
 
 const MainPage = () => {
+  const { t } = useTranslation();
+  const [delBoard] = useDeleteBoardMutation();
+  const handleBoardDelete = async (id: IBoardId) => {
+    await delBoard(id);
+  };
+  const { isLoading, isError, data } = useGetBoardsQuery();
+
   return (
     <motion.div variants={fade} {...motionVariants}>
-      MainPage
+      {isError && <p className="text-center text-red-500">{t('error')}</p>}
+      {isLoading && <p className="text-center text-slate-400">{t('loading')}</p>}
+      <div className="container mx-auto p-6 flex flex-col gap-4">
+        {data?.map((board) => (
+          <Card key={board._id} className="flex justify-between">
+            <h1 className="text-xl">{board.title}</h1>
+            <Button onClick={() => handleBoardDelete(board._id as unknown as IBoardId)}>Del</Button>
+          </Card>
+        ))}
+      </div>
     </motion.div>
   );
 };
