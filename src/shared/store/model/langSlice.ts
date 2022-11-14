3c0412672ec0f223baf687d7ec1ba6324ec10735
Tolegen Timur <demigodkz@gmail.com>
2022-11-15 03:15:37 +0600

    refactor: change structure langSlice

--- src/shared/store/model/langSlice.ts
+++ src/shared/store/model/langSlice.ts
@@ -1,16 +1,18 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+import { getLang, setLang } from 'shared/common/utils';
 import i18n from 'shared/locale/i18n';
 
 type langState = { lang: 'EN' | 'RU' };
 
-const initialState: langState = { lang: 'EN' };
+const initialState: langState = { lang: (getLang() as 'EN' | 'RU') || 'EN' };
 const langSlice = createSlice({
   name: 'lang',
   initialState,
   reducers: {
-    toggleLang(state, { payload }: PayloadAction<'EN' | 'RU'>) {
-      state.lang = payload;
-      i18n.changeLanguage(payload);
+    toggleLang(state) {
+      state.lang = state.lang === 'EN' ? 'RU' : 'EN';
+      setLang(state.lang);
+      i18n.changeLanguage(state.lang);
     },
   },
 });
