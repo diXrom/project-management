2cff37c2fe7a593a1166f37c422387cbb78ec845
Tolegen Timur <demigodkz@gmail.com>
2022-12-05 03:05:27 +0600

    feat: add new link video

--- src/pages/WelcomePage/ui/Features.tsx
+++ src/pages/WelcomePage/ui/Features.tsx
@@ -5,11 +5,11 @@ const Features = () => {
 
   return (
     <section className="container max-w-screen-xl mb-28">
-      <h2 className="text-4xl font-bold text-slate-700 text-center mb-10 ">{t('featTitle')}</h2>
+      <h2 className="mb-10 text-4xl font-bold text-center text-slate-700 ">{t('featTitle')}</h2>
 
       <iframe
         className="m-auto rounded-lg w-full h-[300px] md:w-[700px] sm:h-[380px]"
-        src="https://www.youtube.com/embed/lXA0ATF12JY"
+        src="https://www.youtube.com/watch?v=fKtVUp654cM"
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
