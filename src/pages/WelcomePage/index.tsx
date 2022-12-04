4c6652290587d1dfd0ad13a233d8b45ca0064f40
Tolegen Timur <demigodkz@gmail.com>
2022-12-04 22:17:16 +0600

    feat: add  navigate to boards

--- src/pages/WelcomePage/index.tsx
+++ src/pages/WelcomePage/index.tsx
@@ -5,8 +5,13 @@ import HeroSection from './ui/HeroSection';
 import OurTeam from './ui/OurTeam';
 import Functionality from './ui/Functionality';
 import Features from './ui/Features';
+import { Navigate } from 'react-router-dom';
+import { ROUTE_PATH } from 'shared/common/constants';
+import { getToken } from 'shared/common/utils';
 
 const MainPage = () => {
+  if (getToken()) return <Navigate to={ROUTE_PATH.BOARDS} />;
+
   return (
     <motion.div variants={fade} {...motionVariants}>
       <HeroSection />
